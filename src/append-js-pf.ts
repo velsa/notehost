94050f4b23624b3da2cd9b3720c56cd8be7dd2cd
velsa <velshome@yahoo.com>
2024-02-28 17:11:56 +0200

    alt fix

--- src/append-js-pf.ts
+++ src/append-js-pf.ts
@@ -1,13 +1,13 @@
-import { HTMLRewriter } from '@worker-tools/html-rewriter'
+import { HTMLRewriter } from 'htmlrewriter'
 import { NoteHostSiteConfigFull } from '.'
 import { BodyRewriter, HeadRewriter, MetaRewriter } from './rewriters'
 
 export async function appendJavascriptPolyfill(res: Response, url: URL, config: NoteHostSiteConfigFull) {
   // eslint-disable-next-line no-undef
   return new HTMLRewriter()
-    .on('title', new MetaRewriter(config, url))
-    .on('meta', new MetaRewriter(config, url))
-    .on('head', new HeadRewriter(config))
-    .on('body', new BodyRewriter(config))
+    .on('title', new MetaRewriter(config, url) as any)
+    .on('meta', new MetaRewriter(config, url) as any)
+    .on('head', new HeadRewriter(config) as any)
+    .on('body', new BodyRewriter(config) as any)
     .transform(res)
 }
