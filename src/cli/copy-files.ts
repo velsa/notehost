acf9cf0fbe154603d8b27aea39159dc5bd65524a
velsa <velshome@yahoo.com>
2024-02-28 11:45:20 +0200

    logs

--- src/cli/copy-files.ts
+++ src/cli/copy-files.ts
@@ -12,6 +12,9 @@ interface CopyFilesToSDKParams {
 export async function copyFilesToSDK({ parserConfig, originDir, sdkDir }: CopyFilesToSDKParams) {
   const files = fs.readdirSync(originDir)
 
+  console.log('Copying files to SDK: ', originDir, '->', sdkDir)
+  console.log('Files: ', files)
+
   fs.mkdirSync(sdkDir, { recursive: true })
 
   files.forEach((file) => {
